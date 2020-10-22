f9c9955d2e5c203c944dfd62b3ab5bc3eaa456a2
Graham Keane <40239414+GrahamJamesKeane@users.noreply.github.com>
2020-10-22 21:13:27 +0100

    add main

---
+++ .idea/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstIntitial = 'G';
+        char studnetLastIntitial = 'K';
+        boolean hasPefectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstIntitial);
+        System.out.println(studnetLastIntitial);
+        System.out.println(hasPefectAttendance);
+    }
+}
